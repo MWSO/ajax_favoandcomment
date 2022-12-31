4f8d45b193939829d708ea5e7c861897550b01f7
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2022-12-31 08:37:08 +0000

    follow_start

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
+  has_many :followed_users_talble, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :follower_users_table, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+
+  has_many :followed_user, througt: :followed_users_talble, source: :followed_id
+  has_many :follower_user, througt: :follower_users_table, source: :follower_id
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
