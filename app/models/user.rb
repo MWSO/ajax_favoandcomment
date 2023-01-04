47509333322114f7d1df41734da73c358eecdaf8
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-01-04 12:33:09 +0000

    follow_fixed

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_one_attached :profile_image
   has_many :followed_users, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
-  has_many :follower_users, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followers, through: :followed_users, source: :follower
 
-  has_many :followings, through: :followed_users, source: :followed
-  has_many :followers, through: :follower_users, source: :follower
+  has_many :follower_users, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followings, through: :follower_users, source: :followed
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
@@ -23,17 +23,17 @@ class User < ApplicationRecord
     (profile_image.attached?) ? profile_image : 'no_image.jpg'
   end
 
-  def follow(user_id)
-    followed_users.create(followed_id: user_id)
+  def follow(user)
+    follower_users.create(followed_id: user.id)
   end
 
-  def unfollow(user_id)
-    followed_users.find_by(followed_id: user_id).destroy
+  def unfollow(user)
+    follower_users.find_by(followed_id: user.id).destroy
   end
 
 
   def following?(user)
-    followed_users.include?(user)
+    followings.include?(user)
   end
 
 end
