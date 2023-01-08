9f4859919daff08e6748d841fa378ce28af05048
EC2 Default User <119610386+MWSO@users.noreply.github.com>
2023-01-08 08:04:10 +0000

    asynchoronous_fixed

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -8,6 +8,7 @@ class BookCommentsController < ApplicationController
   end
 
   def destroy
+    @book = Book.find(params[:book_id])
     @comment = BookComment.find(params[:id])
     @comment.destroy
   end
